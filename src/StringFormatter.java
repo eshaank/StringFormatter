3ac9e7181961d17b51a76c1d7f672a2f384da663
dhruvwa16 <League@AdministorsiMac.attlocal.net>
2018-03-25 12:57:04 -0700

    Find number of letters in the ArrayList.
    Read Above

--- src/StringFormatter.java
+++ src/StringFormatter.java
@@ -1,5 +1,26 @@
 
-public class StringFormatter
-{
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+public class StringFormatter {
+	public static void main(String[] args) {
+		StringFormatter format = new StringFormatter();
+		format.totalLetters(Arrays.asList("AP", "COMP", "SCI", "ROCKS"));
+	
+	}
+
+	public static int totalLetters(List<String> wordList) {
+		int length = wordList.size();
+
+		
+		return length;
+	}
+
+
+
+
 
 }
