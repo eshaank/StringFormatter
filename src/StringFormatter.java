531a01c5790b6f549d71d97b0aa86aca216ac449
eshaank <league@WTS-IM-17.attlocal.net>
2018-04-01 12:59:36 -0700

    asdf
    asdf

--- src/StringFormatter.java
+++ src/StringFormatter.java
@@ -8,19 +8,60 @@ import org.omg.Messaging.SYNC_WITH_TRANSPORT;
 public class StringFormatter {
 	public static void main(String[] args) {
 		StringFormatter format = new StringFormatter();
-		format.totalLetters(Arrays.asList("AP", "COMP", "SCI", "ROCKS"));
-	
+		List<String> wordList1 = Arrays.asList("AP", "COMP", "SCI", "ROCKS");
+		List<String> wordList2 = Arrays.asList("GREEN", "EGGS", "AND", "HAM");
+		List<String> wordList3 = Arrays.asList("BEACH", "BALL");
+		// format.totalLetters(wordList1);
+		// format.totalLetters(wordList2);
+		// format.totalLetters(wordList3);
+
+		//format.basicGapWidth(wordList1, 20);
+		//format.basicGapWidth(wordList2, 20);
+		format.basicGapWidth(wordList3, 20);
 	}
 
 	public static int totalLetters(List<String> wordList) {
-		int length = wordList.size();
+		int sum = 0;
 
-		
-		return length;
+		for (String w : wordList) {
+			String str = w.toString();
+			String leng = str.substring(0, str.length());
+			 System.out.println(str.substring(0, str.length()));
+			leng.length();
+			 System.out.println(leng.length());
+			sum += leng.length();
+			 System.out.println(sum);
+
+		}
+		System.out.println(sum);
+		return sum;
+
 	}
 
+	public static int basicGapWidth(List<String> wordList, int i) {
+		int sum = 0;
 
+		for (String w : wordList) {
+			String str = w.toString();
+			String leng = str.substring(0, str.length());
+			 System.out.println(str.substring(0, str.length()));
+			leng.length();
+			 System.out.println(leng.length());
+			sum += leng.length();
+			 System.out.println(sum);
 
+		}
+		System.out.println(sum);
 
+		int leftOver = i - sum;
+
+		System.out.println(leftOver);
+
+		int basic = leftOver / (wordList.size() - 1);
+
+		System.out.println(basic);
+		return basic;
+
+	}
 
 }
--- src/StringFormatterTest.java
+++ src/StringFormatterTest.java
@@ -1,13 +1,10 @@
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-
-
 
 class StringFormatterTest {
 
@@ -16,13 +13,13 @@ class StringFormatterTest {
 		List<String> wordList1 = Arrays.asList("AP", "COMP", "SCI", "ROCKS");
 		List<String> wordList2 = Arrays.asList("GREEN", "EGGS", "AND", "HAM");
 		List<String> wordList3 = Arrays.asList("BEACH", "BALL");
-		assertEquals(14, StringFormatter.totalLetters(wordList1));
-		assertEquals(15, StringFormatter.totalLetters(wordList2));
-		assertEquals(9, StringFormatter.totalLetters(wordList3));
+		 assertEquals(14, StringFormatter.totalLetters(wordList1));
+		 assertEquals(15, StringFormatter.totalLetters(wordList2));
+		 assertEquals(9, StringFormatter.totalLetters(wordList3));
 
-		// assertEquals(2, StringFormatter.basicGapWidth(wordList1, 20));
-		// assertEquals(1, StringFormatter.basicGapWidth(wordList2, 20));
-		// assertEquals(11, StringFormatter.basicGapWidth(wordList3, 20));
+		assertEquals(2, StringFormatter.basicGapWidth(wordList1, 20));
+		assertEquals(1, StringFormatter.basicGapWidth(wordList2, 20));
+		assertEquals(11, StringFormatter.basicGapWidth(wordList3, 20));
 
 	}
 
